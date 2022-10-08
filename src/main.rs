c386d62e7ae121720cb13bf9964a7b62f1092f0d
Andrew Langmeier <raymi306@gmail.com>
2022-10-08 09:03:56 -0400

    Add settings to support arbitrary spritesheet formats

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ use std::collections::HashMap;
 use std::path::PathBuf;
 
 use eframe;
-use eframe::egui::{Vec2, Rect, Pos2};
 use eframe::egui;
+use eframe::egui::{Pos2, Rect, Vec2};
 
 #[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
 struct HashableVec2 {
@@ -29,10 +29,28 @@ impl From<HashableVec2> for egui::widgets::plot::PlotPoint {
     }
 }
 
+// f32's because they're controled by an egui::widgets::DragValue
+struct SpritesheetInfo {
+    sprite_size: u16,
+    num_rows: u8,
+    num_cols: u8,
+}
+
+impl Default for SpritesheetInfo {
+    fn default() -> Self {
+        Self {
+            sprite_size: 32,
+            num_rows: 10,
+            num_cols: 10,
+        }
+    }
+}
+
 struct MyApp {
     save_path: Option<PathBuf>,
     open_path: Option<PathBuf>,
     open_spritesheet_path: Option<PathBuf>,
+    spritesheet_info: SpritesheetInfo,
     spritesheet_handle: Option<egui::TextureHandle>,
     plotted_tiles: HashMap<HashableVec2, Rect>,
     selected_uv: Option<Rect>,
@@ -44,6 +62,7 @@ impl Default for MyApp {
             save_path: None,
             open_path: None,
             open_spritesheet_path: None,
+            spritesheet_info: SpritesheetInfo::default(),
             spritesheet_handle: None,
             plotted_tiles: HashMap::new(),
             selected_uv: None,
@@ -59,7 +78,6 @@ fn pick_file_to(var: &mut Option<PathBuf>) {
 }
 
 impl MyApp {
-
     fn top_panel(&mut self, ctx: &egui::Context) {
         egui::TopBottomPanel::top("my_top_panel").show(ctx, |ui| {
             ui.horizontal(|ui| {
@@ -79,18 +97,38 @@ impl MyApp {
                 if ui.button("Open Spritesheet").clicked() {
                     pick_file_to(&mut self.open_spritesheet_path);
                 }
+                ui.collapsing("Settings", |ui| {
+                    ui.label("Sprite Size");
+                    ui.add(
+                        egui::DragValue::new(&mut self.spritesheet_info.sprite_size)
+                            .clamp_range(1..=1024)
+                            .suffix("px"),
+                    );
+                    ui.label("Num Rows");
+                    ui.add(
+                        egui::DragValue::new(&mut self.spritesheet_info.num_rows)
+                            .clamp_range(1..=255),
+                    );
+                    ui.label("Num Columns");
+                    ui.add(
+                        egui::DragValue::new(&mut self.spritesheet_info.num_cols)
+                            .clamp_range(1..=255),
+                    );
+                });
+
                 if let Some(path) = &self.open_spritesheet_path {
                     let image = image::io::Reader::open(path).unwrap().decode().unwrap();
                     let size = [image.width() as usize, image.height() as usize];
                     let image_buffer = image.to_rgba8();
                     let pixels = image_buffer.as_flat_samples();
-                    let color_image = egui::ColorImage::from_rgba_unmultiplied(size, pixels.as_slice(),);
+                    let color_image =
+                        egui::ColorImage::from_rgba_unmultiplied(size, pixels.as_slice());
                     self.spritesheet_handle = Some(ctx.load_texture(
                         "example-image",
                         color_image,
-                        egui::TextureFilter::Nearest
+                        egui::TextureFilter::Nearest,
                     ));
-                } 
+                }
                 if let Some(handle) = &self.spritesheet_handle {
                     ui.image(handle, handle.size_vec2());
                 }
@@ -99,19 +137,35 @@ impl MyApp {
                     if let Some(handle) = &self.spritesheet_handle {
                         //println!("{:?}", handle.size_vec2()); // 320, 320
                         let handle_size = handle.size_vec2();
-                        for x in (0..handle_size.x as u32).step_by((handle_size.x / 10.0) as usize) {
-                            for y in (0..handle_size.y as u32).step_by((handle_size.y / 10.0) as usize) {
+                        for x in (0..handle_size.x as u32).step_by(
+                            (handle_size.x / self.spritesheet_info.num_rows as f32) as usize,
+                        ) {
+                            for y in (0..handle_size.y as u32).step_by(
+                                (handle_size.y / self.spritesheet_info.num_cols as f32) as usize,
+                            ) {
                                 let normalized_x = x as f32 / handle_size.x;
                                 let normalized_y = y as f32 / handle_size.y;
-                                let max_x = (x as f32 + handle_size.x / 10.0) / handle_size.x;
-                                let max_y = (y as f32 + handle_size.y / 10.0) / handle_size.y;
+                                let max_x = (x as f32
+                                    + handle_size.x / self.spritesheet_info.num_rows as f32)
+                                    / handle_size.x;
+                                let max_y = (y as f32
+                                    + handle_size.y / self.spritesheet_info.num_cols as f32)
+                                    / handle_size.y;
                                 let uv = Rect {
-                                    min: Pos2 { x: normalized_x, y: normalized_y },
+                                    min: Pos2 {
+                                        x: normalized_x,
+                                        y: normalized_y,
+                                    },
                                     max: Pos2 { x: max_x, y: max_y },
                                 };
                                 let mut img_btn = egui::widgets::ImageButton::new(
-                                    handle, Vec2 {x: handle_size.x / 10.0, y: handle_size.y / 10.0}
-                                ).uv(uv);
+                                    handle,
+                                    Vec2 {
+                                        x: handle_size.x / self.spritesheet_info.num_rows as f32,
+                                        y: handle_size.y / self.spritesheet_info.num_cols as f32,
+                                    },
+                                )
+                                .uv(uv);
                                 if self.selected_uv == Some(uv) {
                                     img_btn = img_btn.selected(true);
                                 }
@@ -134,8 +188,12 @@ impl eframe::App for MyApp {
         egui::CentralPanel::default().show(ctx, |ui| {
             egui::plot::Plot::new("level_plot")
                 .data_aspect(1.0)
-                .x_grid_spacer(egui::widgets::plot::uniform_grid_spacer(|_| [100.0, 25.0, 1.0]))
-                .y_grid_spacer(egui::widgets::plot::uniform_grid_spacer(|_| [100.0, 25.0, 1.0]))
+                .x_grid_spacer(egui::widgets::plot::uniform_grid_spacer(|_| {
+                    [100.0, 25.0, 1.0]
+                }))
+                .y_grid_spacer(egui::widgets::plot::uniform_grid_spacer(|_| {
+                    [100.0, 25.0, 1.0]
+                }))
                 .allow_boxed_zoom(false)
                 .allow_drag(false)
                 .allow_double_click_reset(false)
@@ -150,16 +208,24 @@ impl eframe::App for MyApp {
                     }
                     let is_drag = drag_delta.x != 0.0 || drag_delta.y != 0.0;
                     if plot_ui.plot_clicked() || is_drag {
-                        if let (Some(coord), Some(selected_uv)) = (plot_ui.pointer_coordinate(), self.selected_uv) {
+                        if let (Some(coord), Some(selected_uv)) =
+                            (plot_ui.pointer_coordinate(), self.selected_uv)
+                        {
                             let coord_x = coord.x.floor();
                             let coord_y = coord.y.floor();
-                            let point = egui::widgets::plot::PlotPoint {x: coord_x, y: coord_y};
+                            let point = egui::widgets::plot::PlotPoint {
+                                x: coord_x,
+                                y: coord_y,
+                            };
                             if !is_drag {
-                                if let None = self.plotted_tiles.remove(&HashableVec2::from(point)) {
-                                    self.plotted_tiles.insert(HashableVec2::from(point), selected_uv);
+                                if let None = self.plotted_tiles.remove(&HashableVec2::from(point))
+                                {
+                                    self.plotted_tiles
+                                        .insert(HashableVec2::from(point), selected_uv);
                                 }
                             } else {
-                                self.plotted_tiles.insert(HashableVec2::from(point), selected_uv);
+                                self.plotted_tiles
+                                    .insert(HashableVec2::from(point), selected_uv);
                             }
                         }
                     }
@@ -170,8 +236,15 @@ impl eframe::App for MyApp {
                                 x: point.x as f64 + 0.5,
                                 y: point.y as f64 + 0.5,
                             };
-                            let img = egui::widgets::plot::PlotImage::new(handle, final_coord, Vec2 {x: handle_size.x / 10.0, y: handle_size.y / 10.0} / 32.0)
-                                .uv(*uv);
+                            let img = egui::widgets::plot::PlotImage::new(
+                                handle,
+                                final_coord,
+                                Vec2 {
+                                    x: handle_size.x / self.spritesheet_info.num_rows as f32,
+                                    y: handle_size.y / self.spritesheet_info.num_cols as f32,
+                                } / self.spritesheet_info.sprite_size as f32,
+                            )
+                            .uv(*uv);
                             plot_ui.image(img);
                         }
                     }
